9f997a50cf9566da2fac1c2232d106ae43d99738
Arturo Silvelo <arturo.silvelo@gmail.com>
2020-09-02 19:04:28 +0200

    [feature]: unused css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,37 +14,17 @@ export class AppComponent implements OnDestroy {
   mobileQuery: MediaQueryList;
   sidenav: ISidenav[] = [];
   me: IMeInfo = {} as IMeInfo;
-  links = [
-    { title: 'HOME', ref: '/home' },
-    { title: 'ABOUT', ref: '/about-us' },
-    { title: 'EDUCATION', ref: '/education' },
-    { title: 'EXPERIENCE', ref: '/experience' }
-  ];
 
   private _mobileQueryListener: () => void;
 
   constructor(
-    private domSanitizer: DomSanitizer,
     private _apiService: ApiService,
-    private matIconRegistry: MatIconRegistry,
     changeDetectorRef: ChangeDetectorRef,
     media: MediaMatcher
   ) {
     this.mobileQuery = media.matchMedia('(max-width: 720px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
-    this.matIconRegistry.addSvgIcon(
-      'github',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icons/github.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'linkedin',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icons/linkedin.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'twitter',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icons/twitter.svg')
-    );
   }
   async ngOnInit() {
     this.sidenav = await this._apiService.getMenu();
