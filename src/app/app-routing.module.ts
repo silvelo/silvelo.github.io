433365d8e83cec797c8584d923b459fe36522bfd
Arturo Silvelo <arturo.silvelo@gmail.com>
2020-11-08 18:43:48 +0100

    [fix]: remove preload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,7 +26,6 @@ const routes: Routes = [{
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
       scrollPositionRestoration: 'top', useHash: true,
     })
   ],
