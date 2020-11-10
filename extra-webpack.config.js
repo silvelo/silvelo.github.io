28f5c0663ed54f6afd1848fb59da06ad845a8149
Arturo Silvelo <arturo.silvelo@gmail.com>
2020-11-10 16:36:10 +0100

    [feature]: css minified and remove

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,8 +1,19 @@
 const CompressionPlugin = require('compression-webpack-plugin');
 const BrotliPlugin = require('brotli-webpack-plugin');
+const PurgeCSSPlugin = require('purgecss-webpack-plugin')
+
+const glob = require('glob');
 
 module.exports = {
   plugins: [
+
+    new PurgeCSSPlugin({
+      paths: glob.sync('./src/**/*', { nodir: true }),
+      fontFace: true,
+      keyframes: true,
+      variables: true,
+    }),
+
     new BrotliPlugin({
       asset: '[fileWithoutExt].[ext].br',
       test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/,
