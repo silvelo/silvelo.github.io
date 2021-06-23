72d142567711a5a64857f4c9f455b6a4c0acc979
Arturo Silvelo <arturo.silvelo@gmail.com>
2021-06-23 17:43:42 +0200

    [feat] Change layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 import { DefaultComponent } from './default/default.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { ContentLayoutComponent } from './content-layout/content-layout.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 
 
 @NgModule({
   declarations: [
-    DefaultComponent
+    DefaultComponent,
+    ContentLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent
   ],
   imports: [
     CommonModule,
